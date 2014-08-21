7cfecb89104eea5cd0f165d588897bb85556d563
Fabien Bochu <fabien.bochu@polyconseil.fr>
2014-08-21 17:22:40 +0200

    Version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='autoguard',
-    version='0.0.4',
+    version='0.1.0',
 
     description='Configuration setup for sentry',
     long_description=long_description,
